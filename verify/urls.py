94b88c4277e7b7233bb59dc70765ffa98e349a60
Knesh <shakura.va@u-sonic.ru>
2021-04-12 22:20:39 +0300

    optimization of the project structure

--- verify/urls.py
+++ verify/urls.py
@@ -4,16 +4,23 @@ from . import views
 
 app_name = 'verify'
 
+# Главные страницы
 urlpatterns = [
-     path('',
-          views.index,
-          name='index'),
+     path('', views.index, name='index')
+]
+
+# Студенты
+urlpatterns += [
      path('students/',
           views.student_list,
           name='student_list'),
      path('students/<str:username>/',
           views.student_active_check,
           name='student_active_check'),
+]
+
+# Группы
+urlpatterns += [
      path('group/',
           views.group_list,
           name='group_list'),
@@ -23,6 +30,10 @@ urlpatterns = [
      path('group/<slug:slug>/',
           views.group_students,
           name='group_students'),
+]
+
+# Проверки
+urlpatterns += [
      path('user/<str:username>/check_list/',
           views.check_list,
           name='check_list'),
@@ -32,15 +43,6 @@ urlpatterns = [
      path('user/<str:username>/new_check/',
           views.new_check,
           name='new_check'),
-     path('user/<str:username>/<int:check_id>/add_remark/',
-          views.add_remark,
-          name='add_remark'),
-     path('user/<str:username>/<int:check_id>/<int:remark_id>/delete_remark/',
-          views.delete_remark,
-          name='delete_remark'),
-     path('user/<str:username>/<int:check_id>/<int:remark_id>/edit_remark/',
-          views.edit_remark,
-          name='edit_remark'),
      path('user/<str:username>/<int:check_id>/check_view/',
           views.check_view,
           name='check_view'),
@@ -53,4 +55,17 @@ urlpatterns = [
      path('user/<str:username>/<int:check_id>/check_active/',
           views.check_active,
           name='check_active'),
+]
+
+# Замечания
+urlpatterns += [
+     path('user/<str:username>/<int:check_id>/add_remark/',
+          views.add_remark,
+          name='add_remark'),
+     path('user/<str:username>/<int:check_id>/<int:remark_id>/delete_remark/',
+          views.delete_remark,
+          name='delete_remark'),
+     path('user/<str:username>/<int:check_id>/<int:remark_id>/edit_remark/',
+          views.edit_remark,
+          name='edit_remark'),
 ]
