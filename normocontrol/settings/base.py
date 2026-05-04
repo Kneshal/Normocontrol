8a8d13d577fd1936186b95d05797fe362ea32f90
knesh <shakura-main@yandex.ru>
2026-05-04 11:41:46 +0700

    Fix

--- normocontrol/settings/base.py
+++ normocontrol/settings/base.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'users.apps.UsersConfig',
     'verify',
     'sorl.thumbnail',
