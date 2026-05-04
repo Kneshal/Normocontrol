f549a0fe85f6fa3921bd5b28eaefe8a82d444458
knesh <shakura-main@yandex.ru>
2026-05-04 11:56:39 +0700

    Fix

--- normocontrol/settings/prod.py
+++ normocontrol/settings/prod.py
@@ -19,7 +19,7 @@ ALLOWED_HOSTS = ['95.163.242.7', '95-163-242-7.cloudvps.regruhosting.ru']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 # Password validation
